539d5e71680e427b0e3e078e2f67fc30a3b56e9d
bomibear <bomibearly@gmail.com>
2019-08-06 07:55:14 -0700

    add comments to solution

--- Challenges/reverseAnArray/src/ReverseAnArray.java
+++ Challenges/reverseAnArray/src/ReverseAnArray.java
@@ -6,10 +6,12 @@ public class ReverseAnArray {
         System.out.println(Arrays.toString(reverseArray(toBeReversed)));
     }
 
+    //Takes in an array and returns an array
     public static int[] reverseArray(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
-
+        //as long as the start pointer is not greater than the end pointer,
+        //swap the elements:
         while (start < end) {
             int temp = arr[start];
             arr[start] = arr[end];
@@ -17,7 +19,6 @@ public class ReverseAnArray {
             start++;
             end--;
         }
-
         return arr;
     }
 }
