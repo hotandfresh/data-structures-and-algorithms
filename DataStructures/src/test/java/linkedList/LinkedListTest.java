f684956b644d282919b37dd62d3b7c93be4c280c
bomibear <bomibearly@gmail.com>
2019-08-12 09:04:20 -0700

    finish 3 methods

---
+++ DataStructures/src/test/java/linkedList/LinkedListTest.java
@@ -0,0 +1,24 @@
+package linkedList;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+
+    @Test
+    public void canInstantiateEmptyLL(){
+
+    }
+
+    @Test
+    public void canInsertIntoLL(){
+
+    }
+
+//    The head property will properly point to the first node in the linked list
+//    Can properly insert multiple nodes into the linked list
+//    Will return true when finding a value within the linked list that exists
+//    Will return false when searching for a value in the linked list that does not exist
+//    Can properly return a collection of all the values that exist in the linked list
+}
