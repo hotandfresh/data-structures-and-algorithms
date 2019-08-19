043e28c13c705caa3a4e34ba5054c0fc69236379
bomibear <bomibearly@gmail.com>
2019-08-19 09:33:31 -0700

    finish tests and comments

--- DataStructures/src/main/java/stacksandqueues/Queue.java
+++ DataStructures/src/main/java/stacksandqueues/Queue.java
@@ -2,24 +2,42 @@ package stacksandqueues;
 
 public class Queue<T> {
     public Node<T> front;
+    public Node<T> tail;
 
+    //adds a new node to the end of the queue
+    //returns nothing
     public void enqueue(T data){
+        Node<T> newNode = new Node(data);
+        //If there is a tail, point tail.next to the new node
+        if(tail != null){
+            tail.next = newNode;
+        }
+        //whether or not the tail is null, assign the tail as the new node
+        tail = newNode;
+
+        //if there's nothing at the front of the queue, assign the front as the new node
         if(front == null){
-            front = new Node(data);
-        } else {
-            Node<T> newNode = new Node(data);
-            newNode.next = front;
             front = newNode;
         }
     }
 
+    //removes a node from the front of the queue
+    //returns the value that was removed
     public T dequeue(){
         if(front == null){
             throw new NullPointerException();
         }
 
+        //assign the value of the front, so it's not lost
         T data = front.data;
+        //move the front pointer to the next node in line
         front = front.next;
+
+        //if there's nothing, make sure to assign the tail to null as well
+        if(front == null){
+            tail = null;
+        }
+
         return data;
     }
 
--- DataStructures/src/test/java/stacksandqueues/QueueTest.java
+++ DataStructures/src/test/java/stacksandqueues/QueueTest.java
@@ -26,26 +26,30 @@ public class QueueTest {
     public void canEnqueueMultipleValues(){
         Queue<Integer> newQ = new Queue<>();
 
+        //this is the first thing added, so it'll be the front:
         newQ.enqueue(10);
         newQ.enqueue(20);
         newQ.enqueue(30);
+
+        //this is the last thing added, so it'll be the tail:
         newQ.enqueue(40);
 
-        assertTrue(newQ.front.data == 40);
+        assertTrue(newQ.tail.data == 40);
     }
 
     @Test
     public void canDequeue(){
         Queue<Integer> newQ = new Queue<>();
-
+        //this is the front that will be dequeued:
         newQ.enqueue(10);
         newQ.enqueue(20);
-        //this is the front that will be dequeued:
         newQ.enqueue(30);
-        //the node with the 20 will now be the front:
+
+        //the node with the 20 will now be the front
+        //and the noded that was dequeued is the 10
         int result = newQ.dequeue();
 
-        assertTrue(result == 30);
+        assertTrue(result == 10);
     }
 
     @Test(expected = NullPointerException.class)
@@ -76,7 +80,7 @@ public class QueueTest {
         newQ.enqueue(30);
         newQ.enqueue(40);
 
-        assertTrue(newQ.peek() == 40);
+        assertTrue(newQ.peek() == 10);
     }
 
 }
