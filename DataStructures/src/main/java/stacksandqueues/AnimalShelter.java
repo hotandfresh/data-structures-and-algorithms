609b10e20de252904b7a2c7c7c0311a3f586d173
bomibear <bomibearly@gmail.com>
2019-08-21 08:35:39 -0700

    add picture

--- DataStructures/src/main/java/stacksandqueues/AnimalShelter.java
+++ DataStructures/src/main/java/stacksandqueues/AnimalShelter.java
@@ -1,4 +1,24 @@
 package stacksandqueues;
 
-public class AnimalShelter {
+public class AnimalShelter<T> {
+    public Queue<T> queue;
+
+    public AnimalShelter(Queue<T> queue){
+        this.queue = queue;
+    }
+
+    public void enqueue(T data){
+        this.queue.enqueue(data);
+    }
+
+    public T dequeue(T pref){
+        while(this.queue.front != null){
+            if(pref.equals(this.queue.front.data)){
+                return this.queue.dequeue();
+            }
+
+            this.queue.front = this.queue.front.next;
+        }
+        return null;
+    }
 }
