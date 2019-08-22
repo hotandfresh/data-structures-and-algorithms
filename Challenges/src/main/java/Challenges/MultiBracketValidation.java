97b479ed2ebded355d5cd4625d7b2c6c409f2172
bomibear <bomibearly@gmail.com>
2019-08-21 18:37:49 -0700

    add skeleton of file and tests

---
+++ Challenges/src/main/java/Challenges/MultiBracketValidation.java
@@ -0,0 +1,8 @@
+package Challenges;
+
+public class MultiBracketValidation {
+    public boolean multiBracketValidation(String input){
+
+        return true;
+    }
+}
---
+++ Challenges/src/test/java/Challenges/MultiBracketValidationTest.java
@@ -0,0 +1,18 @@
+package Challenges;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MultiBracketValidationTest {
+
+    @Test
+    public void canValidateStringsThatAreBalanced(){
+
+    }
+
+    @Test
+    public void canValidateStringsThatAreNotBalanced(){
+        
+    }
+}
