99665f2b6059e694e6be5fce127aa8a2b36765f0
bomibear <bomibearly@gmail.com>
2019-08-27 09:47:15 -0700

    add solution

--- DataStructures/src/main/java/tree/FizzBuzzTree.java
+++ DataStructures/src/main/java/tree/FizzBuzzTree.java
@@ -1,10 +1,35 @@
 package tree;
 
-public class FizzBuzzTree extends BinaryTree{
+import java.util.ArrayList;
+
+public class FizzBuzzTree<Object> extends BinaryTree{
 
     public FizzBuzzTree(){
         super();
     }
 
+    public ArrayList<Object> fizzBuzzTree(Node<Object> current){
+        if(current == null){
+            return new ArrayList<>();
+        } else{
+            ArrayList<Object> left = fizzBuzzTree(current.left);
+            ArrayList<Object> right = fizzBuzzTree(current.right);
 
+            ArrayList<Object> ans = new ArrayList<>();
+            ans.addAll(left);
+            if((Integer)current.data % 3 == 0 && (Integer)current.data % 5 == 0){
+                current.data = (Object)"FizzBuzz";
+            } else if((Integer) current.data % 3 == 0){
+                current.data = (Object)"Fizz";
+            } else if((Integer) current.data % 5 == 0){
+                current.data = (Object)"Buzz";
+            }
+
+            ans.add((Object)current.data);
+
+            ans.addAll(right);
+
+            return ans;
+        }
+    }
 }
--- DataStructures/src/main/java/tree/Node.java
+++ DataStructures/src/main/java/tree/Node.java
@@ -1,11 +1,11 @@
 package tree;
 
-public class Node<T> {
-    Node<T> left;
-    Node<T> right;
-    T data;
+public class Node<Object> {
+    Node<Object> left;
+    Node<Object> right;
+    Object data;
 
-    public Node(T data){
+    public Node(Object data){
         this.data = data;
     }
 
