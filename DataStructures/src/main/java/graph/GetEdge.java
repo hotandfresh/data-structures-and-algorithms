061a760d6a898f3aa9e0d312aa54c1daf00cbb6b
bomibear <bomibearly@gmail.com>
2019-09-24 11:43:41 -0700

    still needs work

--- DataStructures/src/main/java/graph/GetEdge.java
+++ DataStructures/src/main/java/graph/GetEdge.java
@@ -4,8 +4,26 @@ import java.util.ArrayList;
 
 public class GetEdge {
 
-    public Boolean getEdge(Graph graph, ArrayList<String> cities){
+    public String getEdge(Graph graph, ArrayList<String> cities){
+        int total = 0;
+        for(int i = 0; i < cities.size(); i++){
+            if(i != cities.size()-1){
+                if(graph.allNodes.get(i).value == cities.get(i) && graph.allNodes.contains()){
+                    ArrayList<Edge> allNeighbors = graph.getNeighbors(graph.allNodes.get(i));
+                    total += allNeighbors.get(i).weight;
+                } else {
+                    return "False, $0";
+                }
 
-        return true;
+//                for(Edge e : (ArrayList<Edge>)allNeighbors){
+//                    if(e.node.equals(nextCity)){
+//                        total += e.weight;
+//                    } else {
+//                        return "False, $0";
+//                    }
+//                }
+            }
+        }
+        return "True, $" + total;
     }
 }
--- DataStructures/src/test/java/graph/GetEdgeTest.java
+++ DataStructures/src/test/java/graph/GetEdgeTest.java
@@ -2,14 +2,63 @@ package graph;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class GetEdgeTest {
 
     @Test
-    public void canGetDirectFlight(){}
+    public void canGetDirectFlight(){
+        Graph graph = new Graph();
+        Node node1 = new Node("CHI");
+        Node node2 = new Node("SEA");
+        Node node3 = new Node("PDX");
+        Node node4 = new Node("CLE");
+        Node node5 = new Node("SFO");
+
+        graph.addNode(node1);
+        graph.addNode(node2);
+        graph.addNode(node3);
+        graph.addNode(node4);
+        graph.addNode(node5);
+
+        graph.addEdge(node1, node2, 82);
+        graph.addEdge(node2, node3, 50);
+        graph.addEdge(node4, node5, 100);
+
+        GetEdge ge = new GetEdge();
+        ArrayList<String> cities = new ArrayList<>(Arrays.asList("CHI", "SEA", "PDX", "CLE", "SFO"));
+        String actual = ge.getEdge(graph, cities);
+        System.out.println(actual);
+    }
 
     @Test
-    public void canDetermineIfDirectIsntPossible(){}
+    public void canDetermineIfDirectIsntPossible(){
+        Graph graph = new Graph();
+        Node node1 = new Node("CHI");
+        Node node2 = new Node("SEA");
+        Node node3 = new Node("PDX");
+        Node node4 = new Node("CLE");
+        Node node5 = new Node("SFO");
+
+        graph.addNode(node1);
+        graph.addNode(node2);
+        graph.addNode(node3);
+        graph.addNode(node4);
+        graph.addNode(node5);
+
+        graph.addEdge(node1, node2, 82);
+        graph.addEdge(node1, node3, 50);
+        graph.addEdge(node1, node4, 100);
+        graph.addEdge(node1, node4, 20);
+        graph.addEdge(node1, node5, 200);
+
+        GetEdge ge = new GetEdge();
+        ArrayList<String> cities = new ArrayList<>(Arrays.asList("CHI", "BKK", "SEA", "PDX", "CLE", "SFO"));
+        String actual = ge.getEdge(graph, cities);
+        System.out.println(actual);
+    }
 
 }
